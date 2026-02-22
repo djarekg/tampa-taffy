c6c525098124e511fa4755b7ca29777adab589a4
Dustin Griffith <dustingriffith@outlook.com>
2026-02-22 07:15:29 -0500

    users router: added css file

---
+++ apps/demo/src/routes/users/users.css.ts
@@ -0,0 +1,11 @@
+import { css } from 'lit';
+
+export default css`
+  :host {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    gap: 1rem;
+    inline-size: 100%;
+  }
+`;
--- apps/demo/src/routes/users/users.ts
+++ apps/demo/src/routes/users/users.ts
@@ -2,6 +2,7 @@ import { getUsers } from '@/api/user.api';
 import { Task } from '@lit/task';
 import { safeDefine } from '@tt/core/utils';
 import { html, LitElement } from 'lit';
+import styles from './users.css';
 
 /**
  * @summary A simple users page that demonstrates fetching and displaying a list of users.
@@ -10,6 +11,8 @@ import { html, LitElement } from 'lit';
  * This component is registered as 'app-users' and can be navigated to via the router.
  */
 export class Users extends LitElement {
+  static override styles = [styles];
+
   #usersTask = new Task(
     this,
     async () => getUsers(),
